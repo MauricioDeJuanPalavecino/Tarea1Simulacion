541806c2192e729acc783b640b31b27120d07791
DarkHandler <68122176+DarkHandler@users.noreply.github.com>
2022-04-25 00:27:11 -0400

    f exponencial funcionando

--- numeros_variables/congruencial_mult.py
+++ numeros_variables/congruencial_mult.py
@@ -1,8 +1,8 @@
 class congruencial_mult:
-    def __init__(self, seed, modulo):
+    def __init__(self, seed):
         self.seed = seed
-        self.seed = (self.seed * (7**5)) % ((2**modulo) -1)
-    def generar(self, modulo):
-        num_gen = self.seed / ((2 ** modulo) -1)
-        self.seed = (self.seed * (7 ** 5)) % ((2 ** modulo) -1)
+        self.seed = (self.seed * (7**5)) % ((2**31) -1)
+    def generar(self):
+        num_gen = self.seed / ((2 ** 31) -1)
+        self.seed = (self.seed * (7 ** 5)) % ((2 ** 31) -1)
         return num_gen
